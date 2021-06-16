382427385426d9d5fbf5ec0a7ba1189aee12263e
Oiwawa <78601126+Oiwawa@users.noreply.github.com>
2021-06-16 22:51:10 +0200

    Read Me

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # CV---RADEPONT-Hugo
 
-Always an On-going project.
+Salutations aux braves qui liront ceci, je m'appelle Hugo et voici mon site web.
+Réalisé avec HTML, CSS, et un peu de Javascript pour: le texte mode terminal du début du site, avec la librairie Typed,
+plusieurs éléments avec des effets d'apparitions, avec la librairie AOS
+
+C'est un projet toujours en cours, le responsive est un peu bancale mais je travaille dessus ! 
+
+A venir: 
+
+- Responsive 
+- Switch entre dark et light theme
+- Améliorer le style de certains éléments
--- script.js
+++ script.js
@@ -21,3 +21,31 @@ var typed = new Typed('.typed', {
     backSpeed: 0,
 });
 
+// COMPTEUR
+let compteur = 0;
+$(window).scroll(function(){
+    const top = $('.counter').offset().top
+     - window.innerHeight;
+    if (compteur == 0 && $(window).scrollTop() > top){
+        $('.counter-value').each(function(){
+            let $this = $(this),
+                countTo = $this.attr('data-count');
+            $({
+                countNum: $this.text()
+            }).animate({
+                countNum: countTo
+            },
+            {
+                duration: 5000,
+                easing: 'swing',
+                step: function(){
+                    $this.text(Math.floor(this.countNum));
+                },
+                complete: function(){
+                    $this.text(this.countNum);
+                }
+            });
+        });
+        compteur = 1;
+    }
+});
